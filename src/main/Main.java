2dfa60bc69d40987164895038c2716b3316e6fcc
Zeke Farkas <epf005@bucknell.edu>
2020-04-07 18:01:52 -0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: Zeke Farkas
+ * Section: 11am
+ * Date: 4/7/2020
+ * Time: 5:54 PM
+ *
+ * Project: csci205finalprojectSP2020
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
