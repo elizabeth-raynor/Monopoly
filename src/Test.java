a102092b63d859125e58eef49d194004b513d2d3
Elizabeth Raynor <eyr002@bucknell.edu>
2020-04-14 13:50:41 -0400

    "Test commit from ElizDev"

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: Elizabeth Raynor
+ * Section: YOUR SECTION
+ * Date: 4/14/2020
+ * Time: 1:49 PM
+ *
+ * Project: csci205finalprojectsp2020
+ * Package: main
+ * Class: Test
+ *
+ * Description: Prints "Hello" as a git test
+ *
+ * ****************************************
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
