ae59da40c7d9da2c51352970022e9641e8318ed7
Elizabeth Raynor <eyr002@bucknell.edu>
2020-04-15 16:17:36 -0400

    "Top row done, except for property color bar formatting"

---
+++ src/MonopolyBoard/BoardTiles.java
@@ -0,0 +1,174 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 4/14/2020
+ * Time: 7:53 PM
+ *
+ * Project: csci205finalprojectsp2020
+ * Package: MonopolyBoard
+ * Class: Property
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package MonopolyBoard;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+
+public class BoardTiles extends VBox {
+
+//    /** The width of the node */
+//    private double width;
+//
+//    /** The height of the node */
+//    private double height;
+
+    /** The name of the property */
+    private String sName;
+
+    /** The color of the property */
+    private Paint color;
+
+    /** The price of the property */
+    private int iCost;
+
+    /** The border of the property */
+    private Border border = new Border(new BorderStroke(null, BorderStrokeStyle.SOLID, null, null));
+
+    /**
+     * Constructor for properties
+     * @param color
+     * @param sName
+     * @param iCost
+//     * @param height
+//     * @param width
+     */
+//    public BoardTiles(Paint color, String sName, int iCost, double height, double width) {
+//        this.color = color;
+//        this.sName = sName;
+//        this.iCost = iCost;
+//        this.height = height;
+//        this.width = width;
+//        formatProperty();
+//    }
+
+    public BoardTiles(Paint color, String sName, int iCost) {
+        this.color = color;
+        this.sName = sName;
+        this.iCost = iCost;
+        formatProperty();
+    }
+
+    /**
+     * Constructor for corners, community chest and chance tiles
+     * @param sName
+//     * @param size
+     */
+//    public BoardTiles(String sName, double size) {
+//        this.height = size;
+//        this.width = size;
+//        this.sName = sName;
+//        formatCorners();
+//    }
+    public BoardTiles(String sName) {
+        this.sName = sName;
+        formatCornerChanComm();
+    }
+
+    /**
+     * Constructor for chance and community chest tiles
+     * @param sName
+//     * @param height
+//     * @param width
+     */
+//    public BoardTiles(String sName, double height, double width) {
+//        this.sName = sName;
+//        this.height = height;
+//        this.width = width;
+//        formatChanComm();
+//    }
+
+//    public BoardTiles(String sName) {
+//        this.sName = sName;
+//        formatChanComm();
+//    }
+
+    /**
+     * Constructor for utilities and railroads
+     * @param sName
+     * @param iCost
+     */
+//    public BoardTiles(String sName, int iCost, double height, double width) {
+//        this.sName = sName;
+//        this.iCost = iCost;
+//        this.height = height;
+//        this.width = width;
+//        formatUtRail();
+//    }
+
+    public BoardTiles(String sName, int iCost) {
+        this.sName = sName;
+        this.iCost = iCost;
+        formatUtRail();
+    }
+
+    private void formatProperty() {
+        this.setAlignment(Pos.CENTER);
+        this.setSpacing(this.getHeight()/3);
+//        this.setPrefWidth(this.width);
+//        this.setPrefHeight(this.height);
+        this.getChildren().add(new Rectangle(50, 88/11, this.color));
+        Label lName = getLabel();
+        this.getChildren().add(lName);
+        this.getChildren().add(new Label(Integer.toString(iCost)));
+        this.setBorder(border);
+    }
+
+    private void formatCornerChanComm() {
+        this.setAlignment(Pos.CENTER);
+//        this.setPrefSize(this.height,this.height);
+        Label lName = getLabel();
+        this.getChildren().add(lName);
+        this.setBorder(border);
+    }
+
+//    private void formatChanComm() {
+//        this.setAlignment(Pos.CENTER);
+//        this.setPrefWidth(this.width);
+//        this.setPrefHeight(this.height);
+//        Label lName = getLabel();
+//        this.getChildren().add(lName);
+//        this.setBorder(border);
+//    }
+
+    private void formatUtRail() {
+        this.setAlignment(Pos.CENTER);
+//        this.setPrefWidth(this.width);
+//        this.setPrefHeight(this.height);
+        Label lName = getLabel();
+        this.getChildren().add(lName);
+        this.getChildren().add(new Label(Integer.toString(iCost)));
+        this.setBorder(border);
+    }
+
+    /**
+     * @return Label with the name of the tile
+     */
+    private Label getLabel() {
+        Label lName = new Label(sName);
+        lName.setWrapText(true);
+        lName.setAlignment(Pos.CENTER);
+        lName.setScaleShape(true);
+        return lName;
+    }
+}
--- src/MonopolyBoard/BoardView.java
+++ src/MonopolyBoard/BoardView.java
@@ -32,13 +32,13 @@ public class BoardView {
     private final GridPane root;
 
     /** The root width and height */
-    private final double BOARD_SIZE = 600;
+    private final double BOARD_SIZE = 630;
 
     /** The background color */
     private final Color BACKGROUND = Color.web("#d4ffde");
 
     /** The monopoly name */
-    private Label monopolyName;
+
 
 
     /**
@@ -52,6 +52,22 @@ public class BoardView {
 
         // Fill background of root
         root.setBackground(new Background(new BackgroundFill(BACKGROUND, null, null)));
+
+        // Size the columns and rows
+        ColumnConstraints columnEdge = new ColumnConstraints();
+        columnEdge.setPercentWidth(14);
+        ColumnConstraints columnMiddle = new ColumnConstraints();
+        columnMiddle.setPercentWidth(8);
+        root.getColumnConstraints().addAll(columnEdge, columnMiddle, columnMiddle, columnMiddle,
+                columnMiddle, columnMiddle, columnMiddle, columnMiddle, columnMiddle, columnMiddle, columnEdge);
+
+        RowConstraints rowEdge = new RowConstraints();
+        rowEdge.setPercentHeight(14);
+        RowConstraints rowMiddle = new RowConstraints();
+        rowMiddle.setPercentHeight(8);
+        root.getRowConstraints().addAll(rowEdge, rowMiddle, rowMiddle, rowMiddle, rowMiddle, rowMiddle,
+                rowMiddle, rowMiddle, rowMiddle, rowMiddle, rowEdge);
+
 
 
         initProperties();
@@ -67,11 +83,66 @@ public class BoardView {
     }
 
     public void initProperties() {
-        /** the width of the property tiles */
-        double PROP_WIDTH = BOARD_SIZE / 12.09;
-        /** The height of the property tiles */
-        double PROP_HEIGHT = BOARD_SIZE / 7.72;
-        root.getChildren().add(new PropertyFX(Color.SKYBLUE, "CONNECTICUT AVENUE", 120, PROP_HEIGHT, PROP_WIDTH));
-//        root.add(new PropertyFX(Color.ORANGE, "NEW YORK AVENUE", 200, PROP_HEIGHT, PROP_WIDTH));
+//        /** the width of the property tiles */
+//        double PROP_WIDTH = BOARD_SIZE*.0769;
+//        /** The height of the property tiles */
+//        double PROP_HEIGHT = BOARD_SIZE*.13;
+//        root.add(new BoardTiles("FREE PARKING", PROP_HEIGHT), 0,0);
+//        BoardTiles y1 = new BoardTiles(Color.RED, "KENTUCKY AVENUE", 220, PROP_HEIGHT, PROP_WIDTH);
+//        root.add(y1, 1, 0);
+//        root.add(new BoardTiles("CHANCE", PROP_HEIGHT, PROP_WIDTH), 2, 0);
+//        root.add(new BoardTiles(Color.RED, "INDIANA AVENUE", 220, PROP_HEIGHT, PROP_WIDTH), 3, 0);
+//        root.add(new BoardTiles(Color.RED, "ILLINOIS AVENUE", 220, PROP_HEIGHT, PROP_WIDTH), 4, 0);
+//        root.add(new BoardTiles("B. & O. RAILROAD", 200, PROP_HEIGHT, PROP_WIDTH),5, 0);
+//        root.add(new BoardTiles(Color.YELLOW, "ATLANTIC AVENUE", 260, PROP_HEIGHT, PROP_WIDTH), 6, 0);
+//        root.add(new BoardTiles(Color.YELLOW, "VETNOR AVENUE", 260, PROP_HEIGHT, PROP_WIDTH), 7, 0);
+//        root.add(new BoardTiles("WATER WORKS", 150, PROP_HEIGHT, PROP_WIDTH), 8, 0);
+//        root.add(new BoardTiles(Color.YELLOW, "MARVIN GARDENS", 260, PROP_HEIGHT, PROP_WIDTH), 9, 0);
+//        root.add(new BoardTiles("GO TO JAIL", PROP_HEIGHT), 10, 0);
+
+
+        root.add(new BoardTiles("FREE PARKING"), 0,0);
+
+        BoardTiles kentucky = new BoardTiles(Color.RED, "KENTUCKY AVENUE", 220);
+        kentucky.setRotate(180);
+        root.add(kentucky, 1, 0);
+
+        BoardTiles chance = new BoardTiles("CHANCE");
+        chance.setRotate(180);
+        root.add(chance, 2, 0);
+
+        BoardTiles indiana = new BoardTiles(Color.RED, "INDIANA AVENUE", 220);
+        indiana.setRotate(180);
+        root.add(indiana , 3, 0);
+
+        BoardTiles illinois = new BoardTiles(Color.RED, "ILLINOIS AVENUE", 220);
+        illinois.setRotate(180);
+        root.add(illinois, 4, 0);
+
+        BoardTiles rr1 = new BoardTiles("B. & O. RAILROAD", 200);
+        rr1.setRotate(180);
+        root.add(rr1,5, 0);
+
+        BoardTiles atlantic = new BoardTiles(Color.YELLOW, "ATLANTIC AVENUE", 260);
+        atlantic.setRotate(180);
+        root.add(atlantic, 6, 0);
+
+        BoardTiles ventor = new BoardTiles(Color.YELLOW, "VETNOR AVENUE", 260);
+        ventor.setRotate(180);
+        root.add(ventor, 7, 0);
+
+        BoardTiles water_works = new BoardTiles("WATER WORKS", 150);
+        water_works.setRotate(180);
+        root.add(water_works, 8, 0);
+
+        BoardTiles marvin = new BoardTiles(Color.YELLOW, "MARVIN GARDENS", 260);
+        marvin.setRotate(180);
+        root.add(marvin, 9, 0);
+
+        BoardTiles jail = new BoardTiles("GO TO JAIL");
+        root.add(jail, 10, 0);
+
+
+
     }
 }
