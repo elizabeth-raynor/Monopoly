8d84380eb485cc25772b918d39e8d6c605cda25a
Michelle Pham <mtp006@bucknell.edu>
2020-04-18 10:25:07 -0400

    Update Die class methods to static, merging to Dev

--- src/Die.java
+++ src/Die.java
@@ -3,15 +3,15 @@ import java.util.Random;
 public class Die {
     private static Random randNumRoller = new Random();
     private static Random randNumRollerTwo = new Random();
-    int numSides = 6;
-    int addOne = 1;
+    private static int numSides = 6;
+    private static int addOne = 1;
 
-    public int roll() {
+    public static int roll() {
         int rollNum = randNumRoller.nextInt(numSides);
         return rollNum + addOne;
     }
 
-    public int rollTwo() {
+    public static int rollTwo() {
         int rollNumTwo = randNumRollerTwo.nextInt(numSides);
         return rollNumTwo + addOne;
     }
